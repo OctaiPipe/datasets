a754bdffec6ef56df152dc9079c26bbbdce9abd8
chongshen.ng@t-dab.com <chongshen.ng@t-dab.com>
2022-11-22 13:36:18 +0000

    SANDB01-377 #comment Remove unnecessary files and comments

--- explore_data.py
+++ explore_data.py
@@ -2,13 +2,6 @@
 
 from datasets import load_dataset
 
-# load_dataset("./cmapss/cmapss.py")
-# cmapss = load_dataset("text", data_files={"train": "./cmapss/data/RUL_FD001.txt"})
-# print(type(cmapss))
-# 
-# print(cmapss)
-# dataset = load_dataset("./cmapss")
-
 data_files = {"train": "train_FD001.csv", "test": "test_FD001.csv"}
 dataset = load_dataset("./cmapss", data_files=data_files)
 print(dataset)
